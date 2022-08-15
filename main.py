850baec90029ec37b2ce3410458a4acd6601241b
FUGAZI <fugazi@FUGAZIs-MacBook-Pro.local>
2022-08-16 00:15:33 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+def important_urgent():
+    x = " "
+    x = (input("Enter your important and urgent task \n"))
+    while x != '2':
+        x = + 1
+    else:
+        return x
+
+
+print("Important Urgent Tasks \n", important_urgent())
